07219dee059f1d6b8ad4dc862bc7530a9714a40d
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2024-10-06 21:29:56 +0200

    realized top pannel and added placeholder for heatmap

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,32 +24,50 @@ class _HomePageState extends State<HomePage> {
         ]
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.grey,
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("No smoking days", style: TextStyle(fontSize: 24)),
-                  Text("15", style: TextStyle(fontSize: 48)),
-                  Text("30% to next day", style: TextStyle(fontSize: 15)),
-                  SizedBox(height: 15,),
-                  LinearProgressIndicator(
-                    value: 0.7,
-                    backgroundColor: Colors.white,
-                    valueColor: AlwaysStoppedAnimation(Color.fromARGB(125, 128, 128, 128)),
-                  )
-                ],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.grey,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("No smoking days", style: TextStyle(fontSize: 24)),
+                      Text("15", style: TextStyle(fontSize: 48)),
+                      Text("30% to next day", style: TextStyle(fontSize: 15)),
+                      SizedBox(height: 15,),
+                      LinearProgressIndicator(
+                        value: 0.7,
+                        backgroundColor: Colors.white,
+                        valueColor: AlwaysStoppedAnimation(Color.fromARGB(125, 128, 128, 128)),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.grey,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Placeholder(),
+                ),
+              ),
+            )
+          ],
         ),
       )
     );
